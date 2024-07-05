4c9b64919daf3ee3455fb5b135cb10ab353670b4
neetuvanshkar <neetuvanshkar26@gmail.com>
2024-07-05 19:41:59 +0530

    now i have some changes my local repo and also applied css

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
+# myapp/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.cookie_form, name='cookie_form'),
     path('set_cookies/', views.set_cookies, name='set_cookies'),
     path('get_cookies/', views.get_cookies, name='get_cookies'),
     path('delete_cookies/', views.delete_cookies, name='delete_cookies'),
--- app/views.py
+++ app/views.py
@@ -1,23 +1,41 @@
+# myapp/views.py
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+def cookie_form(request):
+    return render(request, 'app/cookie_form.html')
+
+@csrf_exempt
 def set_cookies(request):
-    response = HttpResponse("Cookies are set.")
-    response.set_cookie('name', 'John Doe', max_age=3600)  # 1 hour
-    response.set_cookie('age', '30', max_age=3600)         # 1 hour
-    response.set_cookie('city', 'New York', max_age=3600)  # 1 hour
-    return response
+    if request.method == 'POST':
+        name = request.POST.get('name', 'Default Name')
+        age = request.POST.get('age', 'Default Age')
+        city = request.POST.get('city', 'Default City')
+        
+        response = HttpResponse("Cookies are set.")
+        response.set_cookie('name', name, max_age=3600)  # 1 hour
+        response.set_cookie('age', age, max_age=3600)    # 1 hour
+        response.set_cookie('city', city, max_age=3600)  # 1 hour
+        return response
+    else:
+        return HttpResponse("Invalid request method.")
 
 def get_cookies(request):
-    name = request.COOKIES.get('name')
-    age = request.COOKIES.get('age')
-    city = request.COOKIES.get('city')
-
+    name = request.COOKIES.get('name', 'Not Set')
+    age = request.COOKIES.get('age', 'Not Set')
+    city = request.COOKIES.get('city', 'Not Set')
+    
     return HttpResponse(f'Name: {name}, Age: {age}, City: {city}')
 
+@csrf_exempt
 def delete_cookies(request):
-    response = HttpResponse("Cookies are deleted.")
-    response.delete_cookie('name')
-    response.delete_cookie('age')
-    response.delete_cookie('city')
-    return response
+    if request.method == 'POST':
+        response = HttpResponse("Cookies are deleted.")
+        response.delete_cookie('name')
+        response.delete_cookie('age')
+        response.delete_cookie('city')
+        return response
+    else:
+        return HttpResponse("Invalid request method.")
